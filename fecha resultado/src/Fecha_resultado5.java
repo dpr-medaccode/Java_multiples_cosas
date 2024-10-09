43d8093f1f8819ef939220465623fdc1776a2ef3
Daniel <dapeperrru999@gmail.com>
2024-10-09 17:49:15 +0200

    resultado 5

---
+++ fecha resultado/src/Fecha_resultado5.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Fecha_resultado5 {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = 7;
+
+        
+
+        
+        
+
+
+
+    }   
+
+
+
+
+
+
+}
