297049f599af7b91f56d3834a8b4b21c5329835c
Daniel <dapeperrru999@gmail.com>
2024-10-15 20:44:26 +0200

    hgkhkjgkg

---
+++ funciones/src/funciones2.java
@@ -0,0 +1,18 @@
+public class funciones2 {
+    public static void main(String[] args) throws Exception {
+
+        
+
+    }
+
+    public static int[] Ordenar5 (int[] num){
+
+        
+
+        
+
+        return
+
+    }
+
+}
---
+++ funciones/src/funciones3.java
@@ -0,0 +1,7 @@
+public class funciones3 {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(4%2);
+
+    }
+}
