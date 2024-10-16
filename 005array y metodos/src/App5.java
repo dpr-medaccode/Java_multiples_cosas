017946de1f4328f35f9f305dadac54241b61b100
Daniel <dapeperrru999@gmail.com>
2024-10-16 11:20:26 +0200

    LOL

--- 005array y metodos/src/App5.java
+++ 005array y metodos/src/App5.java
@@ -1,17 +1,22 @@
 import java.util.Scanner;
+
 public class App5 {
     public static void main(String[] args) throws Exception {
 
         Scanner input = new Scanner(System.in);
 
-        int[] array = {1,2,3,4,5};
+        int[] array = { 1, 2, 3, 4, 5 };
 
-        for(int i = array.length-1; i >= 0; i--){
+        for (int i = array.length - 1; i >= 0; i--) {
 
             System.out.println(array[i]);
 
         }
 
     }
-    
+
 }
+
+// Ardió en el fuego del Armagedón, ardió en el fuego del tormento, eligió el
+// camino del tormento eterno en el cielo. No podía soportarlo. Fue al pueblo a
+// adorar al príncipe negro que mató
