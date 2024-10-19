f4904d4b9fc505a95ba18d7bc13709c64946539f
Daniel <dapeperrru999@gmail.com>
2024-10-19 20:20:35 +0200

    ljl

--- 008algoritmo_de_sorteo/src/Algoritmo2.java
+++ 008algoritmo_de_sorteo/src/Algoritmo2.java
@@ -1,127 +1,62 @@
 import java.util.Scanner;
 
-public class Algoritmo2{
+public class Algoritmo2 {
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
 
         int[] numeros = new int[8];
 
-        for(int j = 0; j < 8; j++){
+        for (int j = 0; j < 8; j++) {
 
-            System.out.println("inserte el "+j+"º numero");
+            System.out.println("inserte el " + (j + 1) + "º numero");
 
             numeros[j] = sc.nextInt();
 
         }
 
-        //System.out.println("inserte el 1º numero");
-        //int num1 = sc.nextInt();
+        for (int i = 100; i != 0; i--) {
 
-        //System.out.println("inserte el 2º numero");
-        //int num2 = sc.nextInt();
+            for (int f = 0; f < numeros.length - 1; f = f + 2) {
 
-        //System.out.println("inserte el 3º numero");
-        //int num3 = sc.nextInt();
+                if (numeros[f] > numeros[f + 1]) {
 
-        //System.out.println("inserte el 4º numero");
-        //int num4 = sc.nextInt();
+                    int num = numeros[f];
 
-        //System.out.println("inserte el 5º numero");
-        //int num5 = sc.nextInt();
+                    numeros[f] = numeros[f + 1];
 
-        //System.out.println("inserte el 6º numero");
-        //int num6 = sc.nextInt();
+                    numeros[f + 1] = num;
 
-        //System.out.println("inserte el 7º numero");
-        //int num7 = sc.nextInt();
-
-        //System.out.println("inserte el 8º numero");
-        //int num8 = sc.nextInt();
-
-        //for (int i = 100; i != 0; i--) {
-
-            ///////////////////////////////////////////////////////////////////////////////////////////////////////
-
-        //   if (num1 > num2) {
-
-        //        int numt1 = num1;
-
-        //        num1 = num2;
-
-        //        num2 = numt1;
-
-        //    }
-
-        //    if (num3 > num4) {
-
-        //        int numt2 = num3;
-
-        //        num3 = num4;
-
-        //        num4 = numt2;
-
-        //    }
-
-        //    if (num5 > num6) {
-
-        /*         
-
-                num5 = num6;
-
-                num6 = numt3;
+                }
 
             }
 
-            if (num7 > num8) {
+            for (int y = 1; y < numeros.length - 2; y = y + 2) {
 
-                int numt4 = num7;
+                if (numeros[y] > numeros[y + 1]) {
 
-                num7 = num8;
+                    int num = numeros[y];
 
-                num8 = numt4;
+                    numeros[y] = numeros[y + 1];
 
-            }
+                    numeros[y + 1] = num;
 
-            ///////////////////////////////////////////////////////////////////////////////////////////////////////
-
-            if (num2 > num3) {
-
-                int numtt1 = num2;
-
-                num2 = num3;
-
-                num3 = numtt1;
-
-            }
-
-            if (num4 > num5) {
-
-                int numtt2 = num4;
-
-                num4 = num5;
-
-                num5 = numtt2;
-
-            }
-
-            if (num6 > num7) {
-
-                int numtt3 = num6;
-
-                num6 = num7;
-
-                num7 = numtt3;
+                }
 
             }
 
         }
 
-        System.out.println(num1 + " / " + num2 + " / " + num3 + " / " + num4 + " / " + num5 + " / " + num6 + " / "
-                + num7 + " / " + num8);
+        for (int a = 0; a < numeros.length; a++) {
 
-                */
+            System.out.print(numeros[a] + "/");
+
+        }
+
     }
 
 }
-    
+
+// el siguiente algoritmo se podria usar para arrays mas largos y ordenar mas
+// numeros, el unico problema que en teoria solo sirve para arrays con una
+// cantidad par de numeros, debido que con impares no funcionaria
