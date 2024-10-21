6428da881f1b1a43e22559b9b7cc19aab0836e92
Daniel <dapeperrru999@gmail.com>
2024-10-21 19:00:00 +0200

    sasc

--- 011palabras/src/palabras2.java
+++
@@ -1,7 +0,0 @@
-public class palabras2 {
-    public static void main(String[] args) throws Exception {
-
-            
-
-    }
-}
---
+++ 012objetos/src/App2.java
@@ -0,0 +1,19 @@
+public class App2 {
+    public static void main(String[] args) throws Exception {
+        
+        Persona p1 = new Persona("Javi", "10/09/1999");
+        Persona p2 = new Persona("Paco", "10/08/1999");
+        
+        
+        Persona[]  personas = {p1, p2};
+
+        for(int i = 0; i < personas.length; i++){
+
+            System.out.println(personas[i]);
+
+        }
+
+    }
+
+}
+
