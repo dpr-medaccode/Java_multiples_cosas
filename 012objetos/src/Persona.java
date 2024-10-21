c091c6d7982e683e9fab9c01ec71d4c9994b4a80
Daniel <dapeperrru999@gmail.com>
2024-10-21 19:17:54 +0200

    ertg

---
+++ 012objetos/src/Persona.java
@@ -0,0 +1,21 @@
+public class Persona {
+
+    private String nombre;
+
+    private String fecha;
+
+    public Persona(String n, String f) {
+
+        this.nombre = n;
+
+        this.fecha = f;
+
+    }
+
+    public String toString() {
+
+        return this.nombre + " " + this.fecha;
+
+    }
+
+}
