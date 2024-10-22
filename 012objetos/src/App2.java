649666bee62b7adf8ea7a69a36bd973ed4fead59
Daniel <dapeperrru999@gmail.com>
2024-10-22 19:53:45 +0200

    cijfjslj

--- 012objetos/src/App2.java
+++ 012objetos/src/App2.java
@@ -4,12 +4,11 @@ public class App2 {
         Persona p1 = new Persona("Javi", "10/09/1999");
         Persona p2 = new Persona("Paco", "10/08/1999");
         
-        
         Persona[]  personas = {p1, p2};
 
         for(int i = 0; i < personas.length; i++){
 
-            System.out.println(personas[i]);
+            System.out.println(personas[i].toString());
 
         }
 
