609a080c34bb438efdf8d24de1459a66e4c52f93
DPR-code <dapeperrru999@gmail.com>
2024-11-11 19:07:59 +0100

    dgfh

--- tarea_tema_7/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        
-        Persona p1 = new Persona("02387423", "maria", "20/30/10", "845iure");
-
-        System.out.println(p1);
-
-    }
-}
