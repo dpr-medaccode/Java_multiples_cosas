35f5242743c7cab80eccf4792ff83c44bbc702d7
DPR-code <dapeperrru999@gmail.com>
2024-12-10 20:23:29 +0100

    g

--- 26polimorfismo/src/App.java
+++ 26polimorfismo/src/App.java
@@ -1,17 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-       
-            SubclassOverload obj = new SubclassOverload();
 
-            obj.demo(10);
-            obj.demo(10, 20);
-            obj.demo(5.5);
+        SubclassOverload obj = new SubclassOverload();
 
-            Overload obj2 = new Overload();
+        obj.demo(10);
+        obj.demo(10, 20);
+        obj.demo(5.5);
 
-            obj2.demo(10);
-            obj2.demo(10, 10);
-            // obj2.demo(5.5);
+        Overload obj2 = new Overload();
+
+        obj2.demo(10);
+        obj2.demo(10, 10);
+        // obj2.demo(5.5);
 
     }
+
 }
